d86bf3f0ce5115c6e30395d8948aae19db06da24
owl <owl@fedora>
2025-09-19 17:40:37 +0330

    Some Edit

--- src/main/java/com/supplychain/base/model/BaseEntityModel.java
+++ src/main/java/com/supplychain/base/model/BaseEntityModel.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
@@ -15,6 +16,7 @@ import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import jakarta.persistence.Version;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -25,10 +27,11 @@ import lombok.ToString;
 @MappedSuperclass
 @Table(name = "Base_Entity_Model")
 @Inheritance(strategy = InheritanceType.JOINED)
+@EqualsAndHashCode(of = "id")
 public abstract class BaseEntityModel<T> {
 
     @Id
-    @GeneratedValue(generator = "EntityModelGenerated")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID", precision = 19, scale = 0)
     T id;
 
